fc9e2b49ef1e56b28d55ad50ecff95fc32254f21
zhumingwu <zhumingwu@hotmail.com>
2022-02-25 17:13:04 +0800

    change

--- code-egg-demo/src/main/java/User.java
+++ code-egg-demo/src/main/java/User.java
@@ -7,7 +7,15 @@ public class User extends BaseEntity {
     Integer userId;
     @Column(remark = "用户名", value = "username", length = 32)
     String userName;
+    @Column(remark = "手机号", value = "mobile_phone", length = 32)
+    String mobilePhone;
+    @Column(remark = "电子邮箱", value = "email_address", length = 32)
+    String emailAddress;
     @Column(remark = "密码", length = 32)
     String password;
+    @Column(remark = "有效始时", value = "from_time", length = 32)
+    String fromTime;
+    @Column(remark = "有效终时", value = "end_time", length = 32)
+    String endTime;
 }
 
